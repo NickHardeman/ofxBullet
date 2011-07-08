aa1f4e2077bfda0bc1212cae109429a02bce629e
Nick Hardeman <nickhardeman@gmail.com>
2011-07-08 14:31:40 -0400

    includes all files used with the addon

---
+++ src/ofxBullet.h
@@ -0,0 +1,31 @@
+/*
+ *  ofxBullet.h
+ *  ofxBullet_v5_sphere_texture
+ *
+ *  Created by Nick Hardeman on 6/3/11.
+ *  Copyright 2011 Arnold Worldwide. All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include "ofxBulletWorldRigid.h"
+#include "ofxBulletConstants.h"
+#include "ofxBulletUtils.h"
+#include "ofxBulletUserData.h"
+
+// shapes //
+#include "ofxBulletSphere.h"
+#include "ofxBulletCylinder.h"
+#include "ofxBulletBox.h"
+#include "ofxBulletBaseShape.h"
+#include "ofxBulletCone.h"
+#include "ofxBulletCapsule.h"
+
+// dynamics //
+#include "ofxBulletJoint.h"
+
+// event data //
+#include "ofxBulletMousePickEvent.h"
+#include "ofxBulletCollisionData.h"
+
