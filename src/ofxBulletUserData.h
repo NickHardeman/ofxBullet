3e2f1f34fba84780ae8695a49fdf5d9158bcc400
Nick Hardeman <nickhardeman@gmail.com>
2011-07-08 14:34:29 -0400

    override this class to pass custom classes with ofxBulletBaseShapes

---
+++ src/ofxBulletUserData.h
@@ -0,0 +1,16 @@
+/*
+ *  ofxBulletUserData.h
+ *  ofxBullet_v3
+ *
+ *  Created by Nick Hardeman on 5/26/11.
+ *  Copyright 2011 Arnold Worldwide. All rights reserved.
+ *
+ */
+
+#pragma once
+
+// extend this class if you wish to pass in custom data for the user data on the rigid body //
+class ofxBulletUserData {
+public:
+	ofxBulletUserData() { }
+};
