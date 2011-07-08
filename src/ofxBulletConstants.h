bdcd4b1d8890366aec08345f801b5680464311dc
Nick Hardeman <nickhardeman@gmail.com>
2011-07-08 14:33:02 -0400

    constants used with addon

---
+++ src/ofxBulletConstants.h
@@ -0,0 +1,22 @@
+/*
+ *  ofxBulletConstants.h
+ *  ofxBullet_v3
+ *
+ *  Created by Nick Hardeman on 5/24/11.
+ *  Copyright 2011 Arnold Worldwide. All rights reserved.
+ *
+ */
+
+#pragma once
+
+// render modes //
+#define OFX_BT_RENDER_QUADS		0
+#define OFX_BT_RENDER_TRIS		1
+
+// Activation States //
+#define OFX_BT_ACTIVATION_STATE_ACTIVE				1
+#define OFX_BT_ACTIVATION_STATE_ISLAND_SLEEPING		2
+#define OFX_BT_ACTIVATION_STATE_WANTS_DEACTIVATION	3
+#define OFX_BT_ACTIVATION_STATE_DISABLE_DEACTIVATION 4
+#define OFX_BT_ACTIVATION_STATE_DISABLE_SIMULATION	5
+
