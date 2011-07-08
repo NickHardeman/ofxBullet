bed0c38931c6c00929bdbc0d65d9cf389bd33f11
Nick Hardeman <nickhardeman@gmail.com>
2011-07-08 14:38:20 -0400

    stores the collision event data

---
+++ src/events/ofxBulletCollisionData.h
@@ -0,0 +1,25 @@
+/*
+ *  CollisionData.h
+ *  ofxBullet_v3
+ *
+ *  Created by Nick Hardeman on 5/25/11.
+ *  Copyright 2011 Arnold Worldwide. All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxBulletBaseShape.h"
+#include "ofxBulletUserData.h"
+
+class ofxBulletCollisionData {
+public:
+	int numContactPoints;
+	ofxBulletUserData* userData1;
+	vector<ofVec3f>	worldContactPoints1;
+	
+	ofxBulletUserData* userData2;
+	vector<ofVec3f>	worldContactPoints2;
+	vector<ofVec3f> normalsOnShape2;
+};
