7aa5ba8215fdb152252616ff3e8fc1a49dca2973
Nick Hardeman <nickhardeman@gmail.com>
2012-05-02 20:56:08 -0400

    removed examples folder so they will compile in add ons folder

---
+++ SimpleExample/src/testApp.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxBullet.h"
+
+class testApp : public ofBaseApp{
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed  (int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	
+	ofxBulletWorldRigid			world;
+	ofxBulletBox				ground;
+	
+	ofxBulletSphere*			sphere;
+	ofxBulletBox*				box;
+	ofxBulletCone*				cone;
+	ofxBulletCapsule*			capsule;
+	ofxBulletCylinder*			cylinder;
+	
+	ofCamera					camera;
+		
+};
