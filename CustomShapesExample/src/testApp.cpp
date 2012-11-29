a6bacc0fd4ccc69dec674c2a54e6935716e83561
Jeff Crouse <jefftimesten@gmail.com>
2012-11-29 10:59:17 -0500

    camera.cacheMatrices() no longer exists

--- CustomShapesExample/src/testApp.cpp
+++ CustomShapesExample/src/testApp.cpp
@@ -8,9 +8,7 @@ void testApp::setup() {
 	
 	camera.setPosition(ofVec3f(0, -3.f, -40.f));
 	camera.lookAt(ofVec3f(0, 0, 0), ofVec3f(0, -1, 0));
-	
-	camera.cacheMatrices(true);
-	
+
 	world.setup();
 	world.enableGrabbing();
 	world.setCamera(&camera);
