afecc44f564f218848de8d37f3c216f24cdcd465
lukasz karluk <julapy@googlemail.com>
2013-09-05 23:08:07 +1000

    added ofxBulletStaticPlane for creating floors, ceilings etc.

---
+++ src/shapes/ofxBulletStaticPlane.cpp
@@ -0,0 +1,57 @@
+//
+//  ofxBulletStaticPlane.cpp
+//  emptyExample
+//
+//  Created by Lukasz Karluk on 18/12/12.
+//
+//
+
+#include "ofxBulletStaticPlane.h"
+#include "btBulletDynamicsCommon.h"
+
+ofxBulletStaticPlane::ofxBulletStaticPlane() {
+    //
+}
+
+ofxBulletStaticPlane::~ofxBulletStaticPlane() {
+    //
+}
+
+void ofxBulletStaticPlane::init(btStaticPlaneShape * shape) {
+    _shape = shape;
+    _bColShapeCreatedInternally = false;
+    _bInited = true;
+}
+
+void ofxBulletStaticPlane::createFloor(btDiscreteDynamicsWorld * world, ofPoint planePosition) {
+    create(world, planePosition, ofPoint(0, -1, 0));
+}
+
+void ofxBulletStaticPlane::createCeiling(btDiscreteDynamicsWorld * world, ofPoint planePosition) {
+    create(world, planePosition, ofPoint(0, 1, 0));
+}
+
+void ofxBulletStaticPlane::create(btDiscreteDynamicsWorld * world, ofPoint planePosition, ofPoint planeNormal, float planeConstant) {
+    btStaticPlaneShape * staticPlaneShape = NULL;
+    if(_bInited == false || _shape == NULL) {
+        staticPlaneShape = new btStaticPlaneShape(btVector3(planeNormal.x, planeNormal.y, planeNormal.z), planeConstant);
+        _bInited = true;
+    } else {
+        staticPlaneShape = (btStaticPlaneShape *)_shape;
+    }
+    
+    btTransform transform = ofGetBtTransformFromVec3f(planePosition);
+    float mass = 0.0;
+    ofxBulletBaseShape::create(world, staticPlaneShape, transform, mass);
+    
+    createInternalUserData();
+}
+
+void ofxBulletStaticPlane::removeShape() {
+    if(_bColShapeCreatedInternally) {
+        if(_shape) {
+            delete (btStaticPlaneShape *)_shape;
+            _shape = NULL;
+        }
+    }
+}
