2be44bd7bde25e473f37a229c3e0086bc975f31d
NickHardeman <nickhardeman@gmail.com>
2020-01-09 15:42:49 -0500

    collision data

--- src/events/ofxBulletCollisionData.h
+++ src/events/ofxBulletCollisionData.h
@@ -15,11 +15,11 @@ class ofxBulletCollisionData {
 public:
 	int numContactPoints;
 	ofxBulletUserData* userData1;
-	vector<glm::vec3>	worldContactPoints1;
+	vector<glm::vec3> worldContactPoints1;
 	const btRigidBody* body1;
 	
 	ofxBulletUserData* userData2;
-	vector<glm::vec3>	worldContactPoints2;
+	vector<glm::vec3> worldContactPoints2;
 	vector<glm::vec3> normalsOnShape2;
 	const btRigidBody* body2;
 };
